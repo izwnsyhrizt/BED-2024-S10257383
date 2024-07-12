a964fafaf07478dad8dd7144e3a6156c81c06241
izwnsyhrizt <izwnsyhrizt@gmail.com>
2024-07-12 20:10:10 +0800

    week 4

---
+++ week04/books-api-mvc-db/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const booksController = require("./controllers/booksController");
+const sql = require("mssql");
+const dbConfig = require("./dbConfig");
+const bodyParser = require("body-parser"); // Import body-parser
+
+
+const app = express();
+const port = process.env.PORT || 3000; // Use environment variable or default port
+
+// Include body-parser middleware to handle JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+// Routes for GET requests (replace with appropriate routes for update and delete later)
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", booksController.createBook); // POST for creating books (can handle JSON data)
+
+
+app.listen(port, async () => {
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig);
+    console.log("Database connection established successfully");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    // Terminate the application with an error code (optional)
+    process.exit(1); // Exit with code 1 indicating an error
+  }
+
+  console.log(`Server listening on port ${port}`);
+});
+
+// Close the connection pool on SIGINT signal
+process.on("SIGINT", async () => {
+  console.log("Server is gracefully shutting down");
+  // Perform cleanup tasks (e.g., close database connections)
+  await sql.close();
+  console.log("Database connection closed");
+  process.exit(0); // Exit with code 0 indicating successful shutdown
+});
---
+++ week04/books-api-mvc-db/controllers/booksController.js
@@ -0,0 +1,43 @@
+const Book = require("../models/book");
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.getAllBooks();
+    res.json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving books");
+  }
+};
+
+const getBookById = async (req, res) => {
+  const bookId = parseInt(req.params.id);
+  try {
+    const book = await Book.getBookById(bookId);
+    if (!book) {
+      return res.status(404).send("Book not found");
+    }
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving book");
+  }
+};
+
+const createBook = async (req, res) => {
+    const newBook = req.body;
+    try {
+      const createdBook = await Book.createBook(newBook);
+      res.status(201).json(createdBook);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error creating book");
+    }
+  };
+  
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+};
